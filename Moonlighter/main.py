cb587d6a958086be428306230d6594b682d7e36e
tlsehdduq <bmbc98@naver.com>
2021-10-21 01:25:04 +0900

    moonlighter
    업데이트

--- Moonlighter/main.py
+++ Moonlighter/main.py
@@ -1,5 +1,8 @@
 from pico2d import *
 import random
+
+
+
 class background:
 
     def __init__(self):
@@ -9,34 +12,48 @@ class background:
 
 class character:
     def __init__(self):
+
         self.image = load_image('character.png')
         self.attack = load_image('characterattack.png')
         self.weapon = load_image('characterweapon.png')
 
         self.x, self.y = 200,300
         self.frame = 0
+        self.a_frame = 0
+        self.w_frame = 0
         self.dir = 0
         self.m_check = True
         self.x_dir = 0
         self.y_dir = 0
+        self.turn = 0
 
     def update(self):
 
         self.frame = (self.frame + 1) % 9
-        # self.frame1 =(self.frame1 + 1) % 9
-        self.x += self.x_dir * 5
-        self.y += self.y_dir * 5
-    def attack(self):
-
         self.a_frame = (self.a_frame + 1) % 6
         self.w_frame = (self.w_frame + 1) % 6
-        self.image.clip_draw(self.frame * 89, self.dir, 100, 100, self.x, self.y)
+        self.x += self.x_dir * 5
+        self.y += self.y_dir * 5
 
     def draw(self):
+        if self.turn == 0:
+            self.image.clip_draw(self.frame * 89, self.dir, 100, 100, self.x, self.y)
+        elif self.turn == 1:
+            self.attack.clip_draw(self.a_frame * 136, 0, 100, 100, self.x, self.y, 65, 100)
+            self.weapon.clip_draw(self.w_frame * 136, 0, 100, 100, self.x, self.y, 65, 80)
+        elif self.turn == 2:
+            self.attack.clip_draw(self.a_frame * 136, 100, 100, 100, self.x, self.y, 65, 100)
+            self.weapon.clip_draw(self.w_frame * 136, 100, 100, 100, self.x, self.y, 65, 80)
+        elif self.turn == 3:
+            self.attack.clip_draw(self.a_frame * 136, 200, 100, 100, self.x, self.y, 65, 100)
+            self.weapon.clip_draw(self.w_frame * 136, 200, 100, 100, self.x, self.y, 65, 80)
+        elif self.turn == 4:
+            self.attack.clip_draw(self.a_frame * 136, 300, 100, 100, self.x, self.y, 65, 100)
+            self.weapon.clip_draw(self.w_frame * 136, 300, 100, 100, self.x, self.y, 65, 80)
 
-        self.image.clip_draw(self.frame * 89, self.dir, 100, 100, self.x, self.y)
-
-
+    # def attackdraw(self):
+    #     self.attack.clip_draw(self.a_frame * 133, self.dir, 100, 100, self.x, self.y)
+    #     self.a_frame(self.a_frame + 1) % 6
     def handle_events(self):
 
         global running
@@ -53,40 +70,55 @@ class character:
                             self.m_check = False
                     elif self.m_check == False:
                         self.x = 180
-
                 if event.key == SDLK_LEFT:
                     self.dir = 200
                     self.x_dir -= 2
-
+                    self.turn = 0
                 if event.key == SDLK_UP:
                     self.dir = 300
                     self.y_dir += 2
+                    self.turn = 0
                 if event.key == SDLK_DOWN:
                     self.dir = 100
                     self.y_dir -= 2
+                    self.turn = 0
+                if event.key == SDLK_j:
+                    if self.dir == 0:
+                        self.turn = 1
+                    elif self.dir == 100:
+                        self.turn = 2
+                    elif self.dir == 200:
+                        self.turn = 3
+                    elif self.dir == 300:
+                        self.turn = 4
+
+
             elif event.type == SDL_KEYUP:
                 if event.key == SDLK_RIGHT:
                     self.x_dir -= 2
+                    self.turn = 0
                 if event.key == SDLK_LEFT:
                     self.x_dir += 2
+                    self.turn = 0
                 if event.key == SDLK_UP:
                     self.y_dir -= 2
+                    self.turn = 0
                 if event.key == SDLK_DOWN:
                     self.y_dir += 2
-
-
-
+                    self.turn = 0
+                if event.key == SDLK_j:
+                    self.turn = 0
 
 class BossMonster:
     def __init__(self):
         self.image = load_image('Boss.png')
     def draw(self):
-        self.image.draw(700, 400)
+        self.image.draw(900, 400)
 
 class Monster1:
     def __init__(self):
         self.image = load_image('Monster1.png')
-        self.x, self.y = random.randint(250,600),random.randint(150,450)
+        self.x, self.y = random.randint(250,900),random.randint(150,450)
         self.check = True
 
     def draw(self):
@@ -99,45 +131,34 @@ class Monster1:
                 self.x -= 10
                 if self.x <= 180: self.check = False
         if self.check == False:
-            if self.x <= 800:
+            if self.x <= 1100:
                 self.x += 10
-                if self.x >=600: self.check = True
+                if self.x >= 1100: self.check = True
 
 
 open_canvas(1280,720)
-
 back = background()
-
 player = character()
-
 plyerattack = character()
-
 boss = BossMonster()
-
-lowmonster = [Monster1() for i in range(3)]
-
+lowmonster = [Monster1() for i in range(5)]
 running = True
 
 while running:
 
     player.handle_events()
-
     player.update()
     for monsters in lowmonster:
         monsters.update()
-
     clear_canvas()
     back.draw()
     player.draw()
     # player.attack()
-
-
-
-
     boss.draw()
+
 
     for monsters in lowmonster:
         monsters.draw()
 
-    delay(0.05)
+    delay(0.03)
     update_canvas()
